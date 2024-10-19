1ba096b81c93669928369641e98df7bb2c6e80e0
Nysa11 <nysakeuk124@gmail.com>
2024-10-19 09:13:42 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from pygame import *
+
+from SpriteClass import GameSprite
+
+''' colors '''
+background = (200, 255, 255)
+
+window = display.set_mode((700, 500))
+window.fill(background)
+
+clock = time.Clock()
+
+
+''' variables '''
+game = True
+
+'''' game loop '''
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    platform_left.reset(window_object=window)
+    platform_right.reset(window_object=window)
+    ball.reset(window_object=window)
+
+    display.update()
+    clock.tick(60)
